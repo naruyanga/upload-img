4b8e610e1c9a063bec0248f956f00ae6e1569c8a
24HP4937@nest.edu.mn <24HP4927@G305-CC0086.local>
2024-10-24 18:01:27 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+const Page = () => {
+  const [img, setImg] = useState(null);
+  const [uploadedImgUrl, setUploadedImgUrl] = useState();
+
+  const uploadImg = async () => {
+    const formData = new FormData();
+    formData.append("file", img);
+    formData.append("cloud_name", "dohdgnsrk");
+    formData.append("upload_preset", "upload");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dohdgnsrk/image/upload",
+      {
+        method: "POST",
+        body: formData,
+      },
+    );
+    const data = await res.json();
+    setUploadedImgUrl(data.secure_url);
+    console.log(data.secure_url);
+    setImg(null);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <input type="file" onChange={(e) => setImg(e.target.files[0])} />
+      <button onClick={() => uploadImg()}>upload</button>
+      {uploadedImgUrl && (
+        <img style={{ height: "600px" }} src={uploadedImgUrl} />
+      )}
     </div>
   );
-}
+};
+export default Page;
